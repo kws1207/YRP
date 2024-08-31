b404f1e031187f79be38f51acd6e919b6bf242dc
yunseokjeong <wjddbstjr03@naver.com>
2024-08-31 19:18:51 +0900

    without testing

--- contract/Yrp.sol
+++ contract/Yrp.sol
@@ -2,43 +2,38 @@
 pragma solidity ^0.8.20;
 
 import "./Ownable.sol";
+import "./DemoHelper.sol";
+import "./Datastructure.sol";
 
 contract Yrp is Ownable {
-    struct Bet {
-        address bettor;
-        int priceRangeIndex;
-        uint value;
-        int amount;
-        bool claimed;
-    }
 
-    mapping(int => Bet[]) public bets;
+    mapping(int => DataStructs.Bet[]) public bets;
     mapping(int => int) public xrpPrices;
     mapping(address => int[]) public betEpochs;
 
     uint256 public baseBetValue = 0.05 ether;
     uint256 public scalingFactor = baseBetValue / 10;
+    int currentEpoch = 9; // TODO: Replace to actual epoch based on block timestamp
+    uint feeConst = 5;
 
-    constructor() {
-        // TODO: Replace with an oracle or other method to fetch actual XRP prices  
-        xrpPrices[1] = 5;
-        xrpPrices[2] = 6;
-        xrpPrices[3] = 4;
+    constructor() Ownable(msg.sender) payable {
+        require(msg.value >= 30, "Not enough volume");
+        DemoHelper.setXrpPrices(xrpPrices); // TODO: Replace with an oracle or other method to fetch actual XRP prices  
+        DemoHelper.setInitialBettings(bets);
     }
 
     function bet(int priceRangeIndex, int betEpoch, int betAmount) external payable {
-        int currentEpoch = 1; // TODO: Replace to actual epoch based on block timestamp
         require(betEpoch > currentEpoch, "Bet epoch must be greater than current epoch");
         require(betEpoch < currentEpoch + 2); // TODO: Extend b-able epoch range
 
-        int requiredValue = (int(baseBetValue) + ((currentEpoch - betEpoch) * int(scalingFactor))) * int(betAmount); // TODO: Consider exponential decay
+        int requiredValue = (int(baseBetValue) + ((currentEpoch + 1 - betEpoch) * int(scalingFactor))) * int(betAmount); // TODO: Consider exponential decay
         require(msg.value == uint(requiredValue), "Incorrect betting amount.");
         
         betEpochs[msg.sender].push(betEpoch);
-        bets[betEpoch].push(Bet({
+        bets[betEpoch].push(DataStructs.Bet({
             bettor: msg.sender,
             priceRangeIndex: priceRangeIndex,
-            value: msg.value,
+            value: msg.value * (1000 - feeConst) / 1000,
             amount: betAmount,
             claimed: false
         }));
@@ -48,29 +43,29 @@ contract Yrp is Ownable {
         uint256 payoutAmount = 0;
 
         for (uint i = 0; i < betEpochs[msg.sender].length; i++) {
-            uint256 totalBetAmountForEpoch = 0;
-            int currentEpoch = betEpochs[msg.sender][i]; // TODO: Check if currentEpoch is completed
-            int actualPriceIndex = xrpPrices[currentEpoch];
-            int totalAmount = 0;
+            uint256 totalVolumeForEpoch = 0;
+            int epoch = betEpochs[msg.sender][i]; // TODO: Check if currentEpoch is completed
+            int actualPriceIndex = int(xrpPrices[epoch] * 100);
+            int validTotalAmount = 0;
             int validUserAmount = 0;
 
-            for (uint j = 0; j < bets[currentEpoch].length; j++) {
-                Bet memory b = bets[currentEpoch][j];
-                if (b.bettor == msg.sender && actualPriceIndex == b.priceRangeIndex && b.claimed == false) {
-                    validUserAmount += b.amount;
-                    bets[currentEpoch][j].claimed = true;
+            for (uint j = 0; j < bets[epoch].length; j++) {
+                DataStructs.Bet memory b = bets[epoch][j];
+                if (actualPriceIndex == b.priceRangeIndex) {
+                    validTotalAmount += b.amount;
+                    if (b.bettor == msg.sender && b.claimed == false) {
+                        validUserAmount += b.amount;
+                        bets[epoch][j].claimed = true;
+                    }
                 }
-                totalAmount += b.amount;
             }
 
-            for (uint j = 0; j < bets[currentEpoch].length; j++) {
-                totalBetAmountForEpoch += bets[currentEpoch][j].value;
+            for (uint j = 0; j < bets[epoch].length; j++) {
+                totalVolumeForEpoch += bets[epoch][j].value;
             }
             
-            payoutAmount += totalBetAmountForEpoch * uint(validUserAmount) / uint(totalAmount);
+            payoutAmount += totalVolumeForEpoch * uint(validUserAmount) / uint(validTotalAmount);
         }
-
-        payoutAmount *= 0.99;
 
         require(payoutAmount <= address(this).balance, "Insufficient funds in contract");
         payable(msg.sender).transfer(payoutAmount);
@@ -84,3 +79,4 @@ contract Yrp is Ownable {
     // Fallback function to accept ETH deposits
     receive() external payable {}
 }
+
